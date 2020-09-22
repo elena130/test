9c41ca008b45d683691a631829c9be41023feeb6
elena <catanzaro.elena01@gmail.com>
2020-09-22 11:37:40 +0200

    aggiunta visualizzazione di tutti i prodotti

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products:any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+      { id: 1, name: 'prodotto1', descrizione: 'descrizione1'},
+      { id: 2, name: 'prodotto2', descrizione: 'descrizione2'},
+      { id: 3, name: 'prodotto3', descrizione: ''},
+    ]
+  }
+
+  share(){
+    alert("Sharing complete!");
   }
 
 }
