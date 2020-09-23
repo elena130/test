2da65d5c6184f9917a5ed85ef15a081661bd8747
elena <catanzaro.elena01@gmail.com>
2020-09-23 10:32:11 +0200

    aggiunta router per il chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailsComponent }
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
