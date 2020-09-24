0f9459e3fcef45645a838f4569b0b3eaec3919c4
elena <catanzaro.elena01@gmail.com>
2020-09-24 10:31:21 +0200

    Aggiunta interfaccia e Shared Folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CoreModule } from './core/core.module';
 import { CartModule } from './cart/cart.module';
 import { ShippingModule } from './shipping/shipping.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     ShippingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,6 @@
+export interface Product{
+    id:number;
+    nome:string;
+    descrizione:string;
+    prezzo:number;
+}
